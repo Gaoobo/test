ced22ee990341868c7c1560323334ccb795896d7
gaoobo <gaooyb@163.com>
2022-08-30 10:14:49 +0800

    创建试图login

--- a/views.py
+++ a/views.py
@@ -1,2 +1,9 @@
 
 from djangp.http import httpresponse
+from django.shortcuts import redirect
+
+def index(request):
+    return HttpResponse("CSDN读者你好啊")
+
+def login(request):
+    return redirect("/index")
